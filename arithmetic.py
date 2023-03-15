5e1f74aee2baac9e7079f6c68043dd73756e1046
Jake Pese <jpiquepese@gmail.com>
2023-03-15 12:38:45 -0700

    Add to new functions

--- arithmetic.py
+++ arithmetic.py
@@ -41,3 +41,13 @@ def mod(num1, num2):
     """Return the remainder of num1 / num2."""
 
     return num1 % num2
+
+def add_mult(num1, num2, num3):
+    """Return the sum of 3 numbers"""
+
+    return (num1 + num2) * num3
+
+def add_cubes(num1, num2):
+    """Return the sum of each number cubed"""
+
+    return (num1 ** 3) + (num2 ** 3)
